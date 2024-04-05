c8ee3ae144f55bbb4d6a788c3a9fcdbffc9602b9
Isaias Cuevas <youngredizzy@hotmail.com>
2024-04-05 12:30:20 -0400

    Task 3 - Updated Converted into a class.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        char choice, ignore;
+        Help hlpobj = new Help();
+
+        for(;;) {
+            do {
+                hlpobj.showMenu();
+                choice = (char) System.in.read();
+
+                do {
+                    ignore = (char) System.in.read();
+                }while (ignore !='\n');
+
+            }while( !hlpobj.isValid(choice));
+
+            if (choice == 'q') break;
+
+            System.out.println("\n");
+            hlpobj.helpOn(choice);
+        }
+    }
+}
